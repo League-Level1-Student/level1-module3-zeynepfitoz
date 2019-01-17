c6928d0e68a5fcac66ddbc1ad1984f7fca90ab5c
league <league@imac-7.attlocal.net>
2019-01-16 18:26:08 -0800

    magic box

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -4,6 +4,7 @@
  */
 
 
+import java.applet.AudioClip;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
@@ -29,9 +30,9 @@ public class MagicBox extends JPanel implements Runnable, MouseListener {
 	 * 
 	 * 3. backgroundImage.getRGB(keyEvent.getX(), keyEvent.getY()) will give you the color of the current pixel.
 	 */
-
+    MediaPalace mp= new MediaPalace();
 	BufferedImage backgroundImage;
-
+	
 	public static void main(String[] args) throws Exception {
 		SwingUtilities.invokeLater(new MagicBox());
 	
@@ -56,6 +57,8 @@ public class MagicBox extends JPanel implements Runnable, MouseListener {
 		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+		frame.addMouseListener(this);
+		
 	}
 
 	private void loadBackgroundImage() throws Exception {
@@ -75,7 +78,8 @@ public class MagicBox extends JPanel implements Runnable, MouseListener {
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+	AudioClip n = mp.loadSound("moo.wav");
+	n.play();
 	}
 
 	@Override
