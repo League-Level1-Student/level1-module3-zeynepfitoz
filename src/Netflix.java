1ff5bb15906b73733fd345e75cbb0566f06aa61c
league <league@wts9.attlocal.net>
2019-01-30 17:22:18 -0800

    netflix

--- src/Netflix.java
+++ src/Netflix.java
@@ -8,6 +8,15 @@ public static void main(String[] args) {
 	Movie tcl = new Movie("10 Cloverfeild Lane", 4);
 	Movie bwp = new Movie("Blair Witch Project", 3);
 	System.out.println(mg.getTicketPrice());
-	NetflixQueue nq = new NetflixQueue
+	NetflixQueue nq = new NetflixQueue();
+	nq.addMovie(i);
+	nq.addMovie(pd);
+	nq.addMovie(mg);
+	nq.addMovie(b);
+	nq.addMovie(tcl);
+	nq.addMovie(bwp);
+	nq.printMovies();
+	System.out.println("the best movie is "+nq.getBestMovie());
+	System.out.println("the second best movie is 10 Cloverfeild Lane");
 }
 }
